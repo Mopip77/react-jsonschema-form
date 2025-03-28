5b73febfad6089eae7488b3698f603a8678494da
mopip77 <mopip77@qq.com>
2025-03-28 12:43:25 +0800

    fix: monaco-editor 强制指定 loader

--- packages/playground/src/index.tsx
+++ packages/playground/src/index.tsx
@@ -1,5 +1,16 @@
 import { render } from 'react-dom';
+import { loader } from '@monaco-editor/react';
 
 import App from './app';
+
+// 配置 Monaco Editor 加载器
+loader.config({
+  paths: {
+    vs: 'https://fastly.jsdelivr.net/npm/monaco-editor@0.43.0/min/vs',
+  },
+  'vs/nls': {
+    availableLanguages: {},
+  },
+});
 
 render(<App />, document.getElementById('app'));
