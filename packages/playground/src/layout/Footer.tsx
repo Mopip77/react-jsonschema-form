c8615bb7894e1d6bb53dacc95c40b5ddc6beb594
mopip77 <mopip77@qq.com>
2025-03-31 21:48:30 +0800

    update changelog

--- packages/playground/src/layout/Footer.tsx
+++ packages/playground/src/layout/Footer.tsx
@@ -15,6 +15,7 @@ export default function Footer() {
       <div style={{ margin: '10px 0', borderRadius: '0.25rem' }}>
         <h4>调整内容</h4>
         <ol>
+          <li>🌟 支持保存示例</li>
           <li>🐞 修复 bigint 精度问题</li>
           <li>🛠️ 【Live validation】默认开启</li>
           <li>🛠️ 数组中没有元素默认不生成数组字段(Populate minItems in arrays -{'>'} requiredOnly)</li>
